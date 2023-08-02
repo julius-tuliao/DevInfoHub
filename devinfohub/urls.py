f6d88e24460031da65b2191f83887ee235dae55f
Julius Tuliao <juliuserictuliao@gmail.com>
2023-08-02 08:06:26 +0800

    added html pages

--- devinfohub/urls.py
+++ devinfohub/urls.py
@@ -1,13 +1,10 @@
 
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-def projects(request):
-    return HttpResponse('Here are our projects')
+from django.urls import path,include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('projects/', projects, name="projects"),
+    path('',include('projects.urls'))
+    
 ]
