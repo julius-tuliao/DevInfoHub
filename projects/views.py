a27a02f4a06f962d31dd00a6fb2fcd9292a8f911
Julius Tuliao <juliuserictuliao@gmail.com>
2023-08-05 23:15:46 +0800

    modified views.py to query projects direct from db also modified templates under project to display projects appropriately

--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project
+
+
 
 def projects(request):
-    return render(request,'projects/projects.html')
+    projects = Project.objects.all()
+
+    context = {'projects': projects}
+    return render(request,'projects/projects.html', context)
 
 def project(request,pk):
+    projectObj = Project.objects.get(id=pk)
 
-    return render(request,"projects/single-project.html")
+    return render(request,"projects/single-project.html", {'project': projectObj})
